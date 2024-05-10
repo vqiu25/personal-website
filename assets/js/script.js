ad0a1d1810c730e1da641dd3f9368a31fac8f9c0
victor <vqiu25@gmail.com>
2024-05-11 11:14:08 +1200

    Functionality: Introduced mobile friendly attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,32 @@
 // Custom Cursor
-document.addEventListener('mousemove', function (e) {
-  const cursor = document.getElementById('cursor');
-  cursor.style.display = 'block';
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
-});
-
-const buttons = document.querySelectorAll('.home-button');
-
-buttons.forEach(button => {
-  button.addEventListener('mouseover', () => {
-    animateCursorSize('2.25rem');
+if (window.innerWidth > 768) {
+  document.addEventListener('mousemove', function (e) {
+    const cursor = document.getElementById('cursor');
+    cursor.style.display = 'block';
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
   });
 
-  button.addEventListener('mouseout', () => {
-    animateCursorSize('1.4rem');
-  });
-});
+  const buttons = document.querySelectorAll('.home-button');
 
-function animateCursorSize(size) {
-  const cursor = document.getElementById('cursor');
-  cursor.style.transition = 'width 0.2s ease, height 0.2s ease';
-  cursor.style.width = size;
-  cursor.style.height = size;
+  buttons.forEach(button => {
+    button.addEventListener('mouseover', () => {
+      animateCursorSize('2.25rem');
+    });
+
+    button.addEventListener('mouseout', () => {
+      animateCursorSize('1.4rem');
+    });
+  });
+
+  function animateCursorSize(size) {
+    const cursor = document.getElementById('cursor');
+    cursor.style.transition = 'width 0.2s ease, height 0.2s ease';
+    cursor.style.width = size;
+    cursor.style.height = size;
+  }
 }
+
 
 
 
