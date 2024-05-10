3807ae7d525b5c872abd78527da11c031d4bfe97
victor <vqiu25@gmail.com>
2024-05-11 00:28:40 +1200

    Cosmetic: Added custom cursor

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Custom Cursor
+document.addEventListener('DOMContentLoaded', function () {
+  const cursor = document.getElementById('cursor');
+  cursor.style.display = 'none';
+});
+
+document.addEventListener('mousemove', function (e) {
+  const cursor = document.getElementById('cursor');
+  cursor.style.display = 'block';
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+});
+
+const buttons = document.querySelectorAll('.home-button');
+
+buttons.forEach(button => {
+  button.addEventListener('mouseover', () => {
+    animateCursorSize('1.9rem');
+  });
+
+  button.addEventListener('mouseout', () => {
+    animateCursorSize('1.4rem');
+  });
+});
+
+function animateCursorSize(size) {
+  const cursor = document.getElementById('cursor');
+  cursor.style.transition = 'width 0.2s ease, height 0.2s ease';
+  cursor.style.width = size;
+  cursor.style.height = size;
+}
+
+
+
